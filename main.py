a79e637aef384d70c84661912575799ea1b4a65f
Svetlana03S <150028901+Svetlana03S@users.noreply.github.com>
2023-11-14 19:04:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+print("Hello")
+
+number = 8
+print(number)
+
+number2 = 8.5
+print(number2)
+
+mySymbol = "D"
+print(mySymbol)
+
+myName = "Svetlana"
+print(myName)
+
+isMan = True
+isWoman = False
+print(isMan)
+print(isWoman)
+
+bin1 = 0b11
+bin2 = 0b1001
+bin3 = 0b1010101010100
+print(bin1)
+print(bin2)
+print(bin3)
+
+oct1 = 0o23645526
+oct2 = 0o10
+print(oct1)
+print(oct2)
+
+hex1 = 0xBABA
+hex2 = 0xDEDA
+print(hex1)
+print(hex2)
+
+com1 = 1 + 4j
+com2 = 78 + 5j
+print(com1)
+print(com2)
+
+str = "Hello \n world"
+print(str)
+
+str1 = "Hello \t\t\t\t\t world"
+print(str1)
+
+str2 = "Hello \t world"
+print(str2)
+
+str3 = "Hello \\ world"
+print(str3)
+
+path = r"C:\Users\Administrutor\PycharmProjects\pythonProject"
+print(path)
+
+userName = "Svetlana"
+userAge = "34"
+userinfo = f"name: {userName} age: {userAge}"
+print(userinfo)
+
+odin = 18
+dva = 5
+summ = odin + dva
+print(summ)
+
+odin = "18"
+dva = "5"
+summ = odin + dva
+print(summ)
+
+print(8+10)
+
+print ("8"+"10")
+
+print("1", 2,6,"yui", True, 0x567)
+
