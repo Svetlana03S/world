21ab464524778ff7689eba1c05ff74861adcd357
Svetlana03S <150028901+Svetlana03S@users.noreply.github.com>
2023-11-20 20:44:03 +0300

    Add files via upload

---
+++ Records.py
@@ -0,0 +1,18 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Administrutor/Desktop/dom.py
+                               Region  ... Total Profit
+0               Australia and Oceania  ...    951410.50
+1   Central America and the Caribbean  ...    248406.36
+2                              Europe  ...    224598.75
+3                  Sub-Saharan Africa  ...     19525.82
+4                  Sub-Saharan Africa  ...    639077.50
+..                                ...  ...          ...
+95                 Sub-Saharan Africa  ...     65214.72
+96                               Asia  ...     15103.47
+97                 Sub-Saharan Africa  ...     93748.05
+98                      North America  ...    144521.02
+99                 Sub-Saharan Africa  ...    889472.91
+
+[100 rows x 14 columns]
