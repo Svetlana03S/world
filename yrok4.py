6710a5599fc43526937c9bc3f894fcaad8a8ff91
Svetlana03S <150028901+Svetlana03S@users.noreply.github.com>
2023-11-24 23:18:48 +0300

    Add files via upload

---
+++ yrok4.py
@@ -0,0 +1,199 @@
+num = 4
+if num ==5:
+    num+=10
+    print("Ravno", num)
+elif num==7:
+    print("Ravno", 7)
+else:
+    print("Num не равно 5, не равно 7")
+    
+#If else
+    
+num = 7
+if num ==5:
+    num+=10
+    print("Ravno", num)
+elif num==7:
+    print("Ravno", 7)
+else:
+    print("Num не равно 5, не равно 7")
+
+cifra = 6
+if cifra ==0:
+    print(0)
+elif cifra ==1:
+    print(1)
+elif cifra ==2:
+    print(2)
+elif cifra ==3:
+    print(3)
+else:
+    print("null civen")
+
+cifra = 3
+if cifra ==0:
+    print(0)
+elif cifra ==1:
+    print(1)
+elif cifra ==2:
+    print(2)
+elif cifra ==3:
+    print(3)
+else:
+    print("null civen")
+    
+num = 5
+if num ==5:
+    num+=10
+    print("Ravno", num)
+    if num==15:
+        print("!")
+elif num==15:
+    print("Ravno", 7)
+else:
+    print("Num не равно 5, не равно 7")
+
+#language = input("Enter language...")
+
+#if language=="ru":
+    print("Russian interface")
+#elif language=="fr":
+    print("Frech interface")
+#else:
+    print("English interface")
+
+if 5!=3:
+    print("5!=3")
+
+a=7
+b=8
+
+if a!=b:
+    print("a!=b")
+else:
+    print("a==b")
+
+    
+a=78
+if a==60:
+    print("a==60")
+elif a>50:
+    print("a>50")
+elif a>=40:
+    print("a>50")    
+elif a<10:
+    print("a>10")
+elif a<=5:
+    print("a>5")
+else:
+    print("aaaaaaaa")
+    
+a=9
+if a==60:
+    print("a==60")
+elif a>50:
+    print("a>50")
+elif a>=40:
+    print("a>50")    
+elif a<10:
+    print("a>10")
+elif a<=5:
+    print("a>5")
+else:
+    print("aaaaaaaa")
+
+a=70
+
+if a>50 and a<59:
+    print("a=(50,59)")
+elif a>50 or a<59:
+    print("a>50 or a<59")
+    
+a=70
+
+if a>50 and a<59:
+    print("a=(50,59)")
+elif a>150 or a==160:
+    print("a>50 or a<59")
+elif not  a==40:
+    print("a is not 40")
+
+message = "hello world!"
+if "hello" in message:
+          print("yes")
+
+if "hello" in message:
+          print("holl" in message)
+
+message = "123456789"
+if "hell" in "hell":
+    print("1" in message)
+
+x1 = 2
+y1 = 5
+z1 = x1 & y1
+print(f"z1={z1}")
+
+a=6
+b=7
+c=a&b
+print(f"c={c}")
+
+x1 = 2
+y1 = 5
+z1 = x1 | y1
+print(f"z1={z1}")
+
+a=16
+b=70
+c=a|b
+print(f"c={c}")
+
+x1 = 2
+y1 = 5
+z1 = x1 ^ y1
+print(f"z1={z1}")
+
+x1 = 3
+y1 = 5
+z1 = x1 ^ y1
+print(f"z1={z1}")
+
+x1 = 6
+y1 = 5
+z1 = x1 ^ y1
+print(f"z1={z1}")
+
+#3^5^5 = 3
+
+x = 8
+y = ~x
+print(y)
+
+x = 0b10
+y = ~x
+print(y)
+
+a=7
+b=1
+c=a>>b
+print(c)
+
+a=7
+b=1
+c=a<<b
+print(c)
+
+a=5
+b=1
+c=a<<b
+print(c)
+
+a=2
+b=4
+c=a>>b
+print(c)
+
+
+
+ 
